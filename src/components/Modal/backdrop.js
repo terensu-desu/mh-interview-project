916f56c7a67fef83994dab6fdd9973d98673d489
Terence Mangram <tmangram@gmail.com>
2018-06-12 14:33:39 -0500

    Change backdrop classname to backdrop

--- src/components/Modal/backdrop.js
+++ src/components/Modal/backdrop.js
@@ -1,7 +1,5 @@
 import React from "react";
 const backdrop = props =>
-  props.show ? (
-    <div className="modalWrapper_backdrop" onClick={props.clicked} />
-  ) : null;
+  props.show ? <div className="backdrop" onClick={props.clicked} /> : null;
 
 export default backdrop;
